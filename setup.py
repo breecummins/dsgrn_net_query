ecf16c394c1f2b65d2fb29bdae1187e6d8dba057
Bree Cummins <breecummins@gmail.com>
2020-07-17 16:07:21 -0600

    fixed url

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     packages = ['dsgrn_net_query',"dsgrn_net_query.queries","dsgrn_net_query.utilities"],
     install_requires=["pandas","mpi4py","progressbar2","DSGRN","min_interval_posets"],
     author="Bree Cummins",
-    url='https://github.com/breecummins/NetworkPerturbations'
+    url='https://github.com/breecummins/dsgrn_net_query'
     )
