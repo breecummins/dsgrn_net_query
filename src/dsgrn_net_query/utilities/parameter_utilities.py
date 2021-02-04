6ed6643fe9ff860660d166bd49f4e3bb13840337
Bree Cummins <breecummins@gmail.com>
2021-02-03 20:44:10 -0700

    neighbor search implemented and tested in CountFP

--- src/dsgrn_net_query/utilities/parameter_utilities.py
+++ src/dsgrn_net_query/utilities/parameter_utilities.py
@@ -1,2 +1,10 @@
-from dsgrn_utilities.get_parameter_neighbors import get_essential_parameter_neighbors
+import DSGRN
+from dsgrn_utilities import get_parameter_neighbors as neighbors
 
+
+def get_neighbors(ess_netspec):
+    noness_net_spec = neighbors.make_nonessential(ess_netspec)
+    noness_pg = DSGRN.ParameterGraph(DSGRN.Network(noness_net_spec))
+    ess_params, nbrs = neighbors.get_essential_parameter_neighbors(noness_pg)
+    paramlist = ess_params + nbrs
+    return noness_net_spec, paramlist
--- tests/test_FP.py
+++ tests/test_FP.py
@@ -15,6 +15,18 @@ def test_count_stableFP():
     time.sleep(1)
 
 
+def test_count_stableFP_neighbors():
+    Path("temp_results").mkdir(exist_ok=True)
+    command = " ".join(["mpiexec", "-n", "2", "python", "../src/dsgrn_net_query/queries/CountFPMatch.py", "mpi_networks_FP.txt", "mpi_params_FPn.json", "temp_results",">dsgrn_net_query.log","2>&1"])
+    os.system(command)
+    qdir = subprocess.check_output("tail -n 1 dsgrn_net_query.log",shell=True).strip().decode("utf-8")
+    output_file = os.path.join(qdir,"query_results.json")
+    results = json.load(open(output_file))
+    assert(results == {'X1 : (X1)(~X3) : E\nX2 : (~X1) : E\nX3 : (X1 + X2) : E\n': [88, 1080], 'X1 : (X1)(~X3) : E\nX2 : (X1) : E\nX3 : (X1 + X2) : E\n': [86, 1080], 'X1 : (X1 + X2) : E\nX2 : (~X3) : E\nX3 : (X2) : E\n': [0, 28]})
+    subprocess.call(["rm","-r", "temp_results/"])
+    time.sleep(1)
+
+
 def test_stableFP2():
     Path("temp_results").mkdir(exist_ok=True)
     command = " ".join(["mpiexec", "-n", "2", "python", "../src/dsgrn_net_query/queries/CountFPMatch.py", "mpi_networks_FP.txt", "mpi_params_FP2.json", "temp_results",">dsgrn_net_query.log","2>&1"])
@@ -52,4 +64,4 @@ def test_multistability2():
 
 
 if __name__ == "__main__":
-    test_multistability2()
+    test_count_stableFP_neighbors()
