ea2bc0cbe57d055eb022bb80df5502fe8132f814
Bree Cummins <breecummins@gmail.com>
2021-02-08 10:15:00 -0700

    bug fix. Returning 2 values now instead of one

--- src/dsgrn_net_query/utilities/parameter_utilities.py
+++ src/dsgrn_net_query/utilities/parameter_utilities.py
@@ -3,7 +3,7 @@ from dsgrn_utilities import get_parameter_neighbors as neighbors
 
 
 def get_neighbors(ess_netspec):
-    noness_net_spec = neighbors.make_nonessential(ess_netspec)
+    ess, noness_net_spec = neighbors.make_nonessential(ess_netspec)
     noness_pg = DSGRN.ParameterGraph(DSGRN.Network(noness_net_spec))
     ess_params, nbrs = neighbors.get_essential_parameter_neighbors(noness_pg)
     paramlist = ess_params + nbrs
